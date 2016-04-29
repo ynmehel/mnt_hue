839d4532aa17879ce6ddce5a4adf138b09ca4176
Yunus Nedim Mehel <yunus.mehel@monitise.com>
2016-04-29 22:47:54 +0300

    Initial commit, prox + networking constatns

---
+++ api_calls.ino
@@ -0,0 +1,102 @@
+
+void setBrightness(int lightNum, int brightness) {
+
+  Serial.println("Setting brightness " + String(brightness));
+  sendCommand(lightNum, "{\"bri\":" + String(brightness) +"}");
+}
+
+void setGroupBrightness(int brightness) {
+
+  Serial.println("Setting group brightness " + String(brightness));
+  sendGroupCommand("{\"bri\":" + String(brightness) +"}");
+}
+
+void turnOnOffLamp(int lightNum, bool isOn) {
+
+  if (isOn) {
+
+    Serial.println("Turning on");
+    sendCommand(lightNum, "{\"on\": true}");
+  }
+  else {
+
+    Serial.println("Turning off");
+    sendCommand(lightNum, "{\"on\": false}");
+  }
+}
+
+void setRandomColorOnGroup() {
+
+  String x = String(random(10, 80) / 100.0);
+  String y = String(random(10, 80) / 100.0);
+  String randomColorCommand = "{\"xy\":[ " + x + "," + y + "]}";
+
+  sendGroupCommand(randomColorCommand );
+  Serial.println("Sending random color to group: " + randomColorCommand);
+
+}
+
+void setRandomColorOnLamp(int lightNum) {
+
+  String x = String(random(10, 80) / 100.0);
+  String y = String(random(10, 80) / 100.0);
+  String randomColorCommand = "{\"xy\":[ " + x + "," + y + "]}";
+
+  sendCommand(lightNum, randomColorCommand );
+  Serial.println("Sending random color: " + randomColorCommand);
+
+}
+
+boolean sendCommand(int lightNum, String command)
+{
+  if (client.connect(hueHubIP, hueHubPort))
+  {
+    while (client.connected())
+    {
+      client.print("PUT /api/");
+      client.print(hueUsername);
+      client.print("/lights/");
+      client.print(lightNum);  // hueLight zero based, add 1
+      client.println("/state HTTP/1.1");
+      client.println("keep-alive");
+      client.print("Host: ");
+      client.println(hueHubIP);
+      client.print("Content-Length: ");
+      client.println(command.length());
+      client.println("Content-Type: text/plain;charset=UTF-8");
+      client.println();  // blank line before body
+      client.println(command);  // Hue command
+    }
+    client.stop();
+    return true;  // command executed
+  }
+  else
+    return false;  // command failed
+}
+
+
+boolean sendGroupCommand(String command)
+{
+  if (client.connect(hueHubIP, hueHubPort))
+  {
+    while (client.connected())
+    {
+      client.print("PUT /api/");
+      client.print(hueUsername);
+      client.print("/groups/0");
+      client.println("/action HTTP/1.1");
+      client.println("keep-alive");
+      client.print("Host: ");
+      client.println(hueHubIP);
+      client.print("Content-Length: ");
+      client.println(command.length());
+      client.println("Content-Type: text/plain;charset=UTF-8");
+      client.println();  // blank line before body
+      client.println(command);  // Hue command
+    }
+    client.stop();
+    return true;  // command executed
+  }
+  else
+    return false;  // command failed
+}
---
+++ prox1.ino
@@ -0,0 +1,102 @@
+/*
+  HC-SR04 Ping distance sensor]
+  VCC to arduino 5v GND to arduino GND
+  Echo to Arduino pin 13 Trig to Arduino pin 12
+  Red POS to Arduino pin 11
+  Green POS to Arduino pin 10
+  560 ohm resistor to both LED NEG and GRD power rail
+  More info at: http://goo.gl/kJ8Gl
+  Original code improvements to the Ping sketch sourced from Trollmaker.com
+  Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+const char hueHubIP[] = "192.168.1.100";  // Hue hub IP
+const char hueUsername[] = "HtBhcb007XSZQnThI9MKRl01gegMJhI0SYdn0hL4";
+const int hueHubPort = 80;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 105); // Arduino IP
+
+const int SOUND_SENSOR = A0; //pin for the Grove Sound Sensor
+
+const int trigPin = 11;
+const int echoPin = 10;
+
+const int maxDistance = 30;
+const int numOfLamps = 3;
+const int maxBrightness = 254;
+
+int loopCounter;
+
+EthernetClient client;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+
+  if (loopCounter == 20) {
+
+    checkDistance();
+    
+    loopCounter = 0;
+  }
+  loopCounter++;
+  delay(10);
+}
+
+
+void checkDistance() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+  if (distance < 4) {
+  }
+
+  if (distance >= maxDistance || distance <= 0) {
+    Serial.println("Out of range");
+  }
+  else {
+
+    int currentLampId = (distance / 10) + 1;
+    int brightness = (distance % 10) * 25.4;
+    Serial.print(distance);
+    Serial.println();
+    for (int i = 1; i <= numOfLamps ; i++) {
+      if (i < currentLampId) {
+        Serial.print("set lamp: ");
+        Serial.print(i);
+        Serial.print(" brightness: ");
+        Serial.print(maxBrightness);
+      }
+      else if (i == currentLampId) {
+        Serial.print("set lamp: ");
+        Serial.print(i);
+        Serial.print(" brightness: ");
+        Serial.print( brightness);
+      }
+      else {
+        Serial.print("turn of: ");
+        Serial.print(i);
+      }
+      Serial.println();
+    }
+  }
+}
+
+
+
+
+
+
